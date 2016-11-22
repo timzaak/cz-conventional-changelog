0d1d8b641df300801f1a0f8de50ce5fbd1153be4
timzaak <zsy.evan@gmail.com>
2016-11-22 10:48:12 +0800

    feat customize the project to support company need

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 "format cjs";
 
 var engine = require('./engine');
-var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = engine({
-  types: conventionalCommitTypes.types
+  types: {
+    "api add": {
+      "description": "add new api"
+    },
+    "api change": {
+      "description": "change api url or api response or api request param"
+    },
+    "api delete": {
+      "description": "delete unused api"
+    },
+    "feat": {
+      "description": "A new feature"
+    },
+    "fix": {
+      "description": "A bug fix"
+    },
+    "refactor": {
+      "description": "A code change that neither fixes a bug nor adds a feature"
+    },
+    "perf": {
+      "description": "A code change that improves performance"
+    },
+    "test": {
+      "description": "Adding missing tests or correcting existing tests"
+    },
+    "build": {
+      "description": "Changes that affect the build system or external dependencies"
+    },
+    "ci": {
+      "description": "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)"
+    },
+    "chore": {
+      "description": "Other changes that don't modify src or test files"
+    },
+    "revert": {
+      "description": "Reverts a previous commit"
+    }
+  }
 });
